13c33277207f20178bef4422d5b9ba7c1ce83013
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-06-11 16:19:59 +0200

    Added vertical orderer class for arcs

---
+++ tests/Sphere_Sweep_Line/vertical_orderer.h
@@ -0,0 +1,22 @@
+#ifndef VERTICAL_ORDERER_H
+#define VERTICAL_ORDERER_H
+
+#include <set>
+
+template <typename K>
+class Vertical_ordered
+{
+    public:
+        // TODO contain/insert/delete operations
+        
+    private:
+        // TODO write compare functor
+        typedef std::less<typename K::Circular_arc_3>
+            Comp_arcs_vertically;
+
+        // BST of arcs sorted vertically
+        std::set<typename K::Circular_arc_3,
+            Comp_arcs_vertically> _arcs;
+};
+
+#endif
