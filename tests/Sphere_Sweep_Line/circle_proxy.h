53ad7bc59feb25686ced9c31e5730401aa62fd5c
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-06-13 14:27:19 +0200

    Implemented both handles for circle proxy

--- tests/Sphere_Sweep_Line/circle_proxy.h
+++ tests/Sphere_Sweep_Line/circle_proxy.h
@@ -17,48 +17,65 @@ template <typename Kernel>
 class Circle_proxy;
 
 template <typename Kernel>
+class Circular_arc_handle;
+
+template <typename Kernel>
 class Circle_handle
 {
   public:
-    // Split the circle
-    //
-    // Concept: points given are on the circle
+    // Split the circle, requires concept that point(s)
+    // given are on the circle
     //
     // ...using one point
-    typename Kernel::Circular_arc_3
-    split(typename Kernel::Point_3 const & p)
-    {
-      typename Kernel::Circular_arc_3 arc(_circle, p);
-      _cp.circle_handle_split(*this, arc);
-      return arc;
-    }
+    Circular_arc_handle<Kernel> split(typename Kernel::Point_3 const & p)
+    { return _cp.split_circle(*this, p); }
     // ...using two points
-    typename Kernel::Circular_arc_3
-    split(typename Kernel::Point_3 const & source,
+    Circular_arc_handle<Kernel> split(typename Kernel::Point_3 const & source,
         typename Kernel::Point_3 const & target)
-    {
-      typename Kernel::Circular_arc_3 arc(_circle, source, target);
-      _cp.circle_handle_split(*this, arc);
-      return arc;
-    }
+    { return _cp.split_circle(*this, source, target); }
 
-    typename Kernel::Circle_3 const & circle() const
+    typename Kernel::Circle_3 const & get() const
     { return _circle; }
 
   private:
     // Bidirectional exclusive access
     friend class Circle_proxy<Kernel>;
 
+    // Can only be constructed from proxy
     Circle_handle(Circle_proxy<Kernel> & cp,
         typename Kernel::Circle_3 const & circle):
       _cp(cp), _circle(circle) {}
 
-    typedef std::vector<boost::reference_wrapper<
-      const typename Kernel::Circular_arc_3> > Arc_list;
-
+    // Reference to parent
     Circle_proxy<Kernel> & _cp;
+    // Underlying circle
     typename Kernel::Circle_3 const & _circle;
-    Arc_list _arcs;
+};
+
+template <typename Kernel>
+class Circular_arc_handle
+{
+  public:
+    typename Kernel::Circular_arc_3 const & get() const
+    { return _arc; }
+
+    // Get the circle handle linked to this arc
+    Circle_handle<Kernel> circle()
+    { return _cp.make_circle_handle(_arc.supporting_circle()); }
+
+  private:
+    // Bidirectional exclusive access
+    friend class Circle_proxy<Kernel>;
+
+    // Can only be constructed from proxy
+    Circular_arc_handle(Circle_proxy<Kernel> & cp,
+        typename Kernel::Circular_arc_3 const & arc):
+      _cp(cp), _arc(arc) {}
+
+    // Reference to parent
+    Circle_proxy<Kernel> & _cp;
+    // Underlying arc
+    typename Kernel::Circular_arc_3 const & _arc;
 };
 
 template <typename Kernel>
@@ -80,16 +97,16 @@ class Circle_proxy
         throw std::logic_error(oss.str());
       }
 #endif // NDEBUG //
-      return make_circle_handle(it_pair.first);
+      return Circle_handle<Kernel>(*this, it_pair.first);
     }
 
   private:
     // Bidirectional exclusive access
     friend class Circle_handle<Kernel>;
+    friend class Circular_arc_handle<Kernel>;
 
     // Set of circles
-    typedef std::set<typename Kernel::Circle_3>
-      Circle_list;
+    typedef std::set<typename Kernel::Circle_3> Circle_list;
 
     // Circle iterators
     // ...usual
@@ -99,10 +116,47 @@ class Circle_proxy
     typedef typename Circle_list::const_iterator
       Circle_const_iterator;
 
-    void circle_handle_split(Circle_handle<Kernel> & ch,
-        typename Kernel::Circular_arc_3 const & arc)
+    // List of arcs
+    typedef std::vector<typename Kernel::Circular_arc_3> Arc_list;
+    Arc_list _arcs;
+
+    Circle_handle<Kernel> make_circle_handle(typename
+        Kernel::Circle_3 const & circle) const
     {
-      // TODO
+      Circle_const_iterator it = _circle_list.find(circle);
+#ifndef NDEBUG
+      if (it == _circle_list.end())
+      {
+        std::ostringstream oss;
+        oss << "Cannot make circle handle from circle which isn't"
+          << " present in the circle proxy";
+        throw std::logic_error(oss.str());
+      }
+#endif // NDEBUG //
+      return Circle_handle<Kernel>(*it);
+    }
+
+    Circular_arc_handle<Kernel> split_circle(Circle_handle<Kernel> & ch,
+        typename Kernel::Point_3 const & p)
+    {
+      typedef typename Kernel::Circular_arc_3 Arc;
+      _arcs.push_back(Arc(ch.get(), p));
+      Arc const & inserted_arc = _arcs.back();
+      return Circular_arc_handle<Kernel>(*this, inserted_arc);
+    }
+
+    std::pair<Circular_arc_handle<Kernel>,
+      Circular_arc_handle<Kernel> > split_circle(Circle_handle<Kernel> & ch,
+        typename Kernel::Point_3 const & source,
+        typename Kernel::Point_3 const & target)
+    {
+      typedef typename Kernel::Circular_arc_3 Arc;
+      _arcs.push_back(Arc(ch.get(), source, target));
+      Arc const & first_arc = _arcs.back();
+      _arcs.push_back(Arc(ch.get(), target, source));
+      Arc const & second_arc = _arcs.back();
+      return std::make_pair(Circular_arc_handle<Kernel>(*this, first_arc),
+          Circular_arc_handle<Kernel>(*this, second_arc));
     }
 
     // Actual set of circles
