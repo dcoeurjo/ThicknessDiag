5058ff9ff00fe85806fe0ba2a17c4851c1051ccb
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-06-21 11:14:16 +0200

    Moved handle class in seperate header

---
+++ tests/Sphere_Sweep_Line/Handle.h
@@ -0,0 +1,95 @@
+#ifndef HANDLE_H
+#define HANDLE_H
+
+#include <iterator>
+
+#define DELEGATE_COMPARAISON_OPERATORS(cl, mem) \
+  bool operator<(const cl & o) const            \
+  { return mem < o.mem; }                       \
+  bool operator>(const cl & o) const            \
+  { return mem > o.mem; }                       \
+  bool operator<=(const cl & o) const           \
+  { return mem <= o.mem; }                      \
+  bool operator>=(const cl & o) const           \
+  { return mem >= o.mem; }                      \
+  bool operator==(const cl & o) const           \
+  { return mem == o.mem; }                      \
+  bool operator!=(const cl & o) const           \
+  { return mem != o.mem; }
+
+// Handle class, emulating pointer functionality
+// while hiding ownership (a handle doesn't own the
+// pointed member object)
+template <typename T>
+class Handle
+{
+  public:
+    typedef T Type;
+
+    Handle():
+      _t(0) {}
+
+    Handle(Type & t):
+      _t(&t) {}
+
+    bool is_null() const
+    { return _t == 0; }
+
+    Type * const ptr() const
+    { return _t; }
+
+    Type & ref() const
+    { return *_t; }
+
+    Type & operator*() const
+    { return ref(); }
+
+    Type * const operator->() const
+    { return ptr(); }
+
+    Type * const operator&() const
+    { return ptr(); }
+
+    DELEGATE_COMPARAISON_OPERATORS(Handle<T>, _t)
+
+  private:
+    Type * _t;
+};
+
+template <typename Iterator>
+class Handle_iterator:
+  public std::iterator<std::input_iterator_tag,
+  Handle<typename Iterator::value_type> >
+{
+  typedef Iterator It;
+  typedef Handle_iterator<It> Self;
+
+  public:
+  typedef Handle<typename Iterator::value_type> Handled;
+
+  Handle_iterator(const It & it):
+    _it(it) {}
+
+  Self & operator++()
+  { ++_it; return *this; }
+
+  Self operator++(int)
+  { Self tmp(*this);
+    ++(*this); return tmp; }
+
+  bool operator==(const Self & sit) const
+  { return _it == sit._it; }
+
+  bool operator!=(const Self & sit) const
+  { return !(*this == sit); }
+
+  Handled operator*()
+  { return Handled(*_it); }
+
+  private:
+  It _it;
+};
+
+#undef DELEGATE_COMPARAISON_OPERATORS // FIXME
+
+#endif // HANDLE_H //
