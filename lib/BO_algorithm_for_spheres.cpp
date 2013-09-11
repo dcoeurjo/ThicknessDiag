dbfbe2268b5f3b63ef9c94e33ff2b37c11c7cfa1
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-11 10:48:35 +0200

    Fixed compile issue

--- lib/BO_algorithm_for_spheres.cpp
+++ lib/BO_algorithm_for_spheres.cpp
@@ -1,5 +1,4 @@
 #include "kernel.h"
-#include <BO_algorithm_for_spheres.h>
 #include <BO_algorithm_for_spheres.ih>
 
 template class BO_algorithm_for_spheres<SK>;
--- lib/Sphere_intersecter.cpp
+++ lib/Sphere_intersecter.cpp
@@ -1,5 +1,4 @@
 #include "kernel.h"
-#include <Sphere_intersecter.h>
 #include <Sphere_intersecter.ih>
 
 template class Sphere_intersecter<SK>;
